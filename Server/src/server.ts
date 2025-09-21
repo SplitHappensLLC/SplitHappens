764ff24b02dcc7ffd63af909fa14624e5777f86b
Lilli Werner <ldwerner@usc.edu>
2025-09-21 15:38:16 -0400

    pending changes to server.ts

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import apiRouter from 
+// import apiRouter from ;
 import { Pool } from 'pg';
 import path from 'path'  // add for serving static files 
 
@@ -10,15 +10,15 @@ const PORT = process.env.PORT || 3000;
 // * set up postreSQL connection pool using pg (for supabase db) * //
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } // necessary for supabase
+    ssl: { rejectUnauthorized: false } // necessary for supabase connect
   });
 
 // * middleware to parse JSON & URL-encoded bodies * //
 app.use(express.json());
 app.use(express.urlencoded( { extended: true }));
 
-// * handle reqs for static files, assuming vite builds to Client/dist ?  * //
-app.use(express.static(path.resolve(import.meta.dirname, '../../Client/dist'))); 
+// * handle reqs for static files, assuming vite builds to '../../Client/dist' ?  * //
+app.use(express.static(path.resolve(import.meta.dirname, '')));  //
 
 
 // * error handling for DB queries * //
@@ -34,9 +34,15 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
 app.get('/api/users/:id', async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-        
+        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]); // $1 is placeholder in sql, way to refer to dynamic data in query. placeholders are numbered from $1 for first param, $2 for second, etc. 
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'User not found'});
+        }
+        res.json(result.rows[0]);
+    } catch(err) {
+        res.status(500).json({ error: (err as Error).message });
     }
-})
+});
 
 
 // create a new user *** how to integrate with supabase auth for secure user creation??? 
