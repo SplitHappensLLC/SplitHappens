cf8b606939cd4a9b68198d3d58ca5c8b3f7e6a30
Christopher Sweeten <chrissweeten0402@gmail.com>
2025-09-21 14:26:56 -0700

    Work on additional UI/UX

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,16 @@
-// import { useState } from 'react'
-import { Routes, Route, Navigate,  BrowserRouter } from 'react-router-dom';
+import { useState } from 'react'
+import { Routes, Route, Navigate,  BrowserRouter, Link } from 'react-router-dom';
 import './App.scss'
 import Home from './pages/Homepage/Home'
 import Login from './pages/Login/Login';
 import CreateUser from  './pages/CreateUser/CreateUser'
+import Profile from './pages/Profile/Profile';
 
 function App() {
+  const isLoggedIn = true
+  const [open, setOpen] = useState(false)
+  const [profileImage, setProfileImage] = useState(null)
 
-  const isLoggedIn = false
 
   return (
 <BrowserRouter>
@@ -16,14 +19,25 @@ function App() {
     <nav className='navbar-container'>
       {isLoggedIn ? <div id='nav-blank'></div> : ""}
       <h1>Split Happens</h1>
-      {isLoggedIn ? <div id='nav-profile-icon'></div>: "" }
+      {isLoggedIn ? <div className="nav-profile-icon" style={{backgroundImage: `url(${profileImage || ""})`}} onClick={() => setOpen(!open)}></div>: "" }
     </nav>
   </section>
   <section className='body-wrapper'>
+    <div className={`sidebar-wrapper ${open ? "is-open" : ""}`}>
+      <div className='sidebar-container'>
+        <li><Link to='/profile'>Profile</Link></li>
+        <hr></hr>
+        <li><Link>History</Link></li>
+        <hr></hr>
+        <li><Link>Friends</Link></li>
+        <hr></hr>
+      </div>
+    </div> 
     <Routes>
       <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="login"/>}/>
       <Route path="/login" element={<Login />}/>
       <Route path="/create-account" element={<CreateUser />}/>
+      <Route path="/profile" element={<Profile setProfileImage={setProfileImage} />} />
     </Routes>
   </section>
 </div>
--- client/pages/Homepage/Home.tsx
+++ client/pages/Homepage/Home.tsx
@@ -7,10 +7,14 @@ import "./Home.scss"
 const Home = () => {
 // 
     return (
-        <>
-
-        </>
-        
+        <div className="homepage-wrapper">
+            <div className="create-group-container">
+                <button className="create-room-btn">Create Room</button>
+            </div>
+            <section className="groups-list-container">
+                <h2 className="open-groups-heading">Open Groups With Friends</h2>
+            </section>
+        </div>
     )
 }
 
