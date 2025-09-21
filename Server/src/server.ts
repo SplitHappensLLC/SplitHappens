e6cbb71dd75d7b7784f4603bb4668f3afde739c6
Colin <colinmontes94@gmail.com>
2025-09-21 14:50:31 -0400

    Co-authored-by: ldwerner <ldwerner@users.noreply.github.com>

---
+++ Server/src/server.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import apiRouter from 
+import { Pool } from 'pg';
+import path from 'path'  // add for serving static files 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// * set up postreSQL connection pool using pg (for supabase db) * //
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false } // necessary for supabase
+  });
+
+// * middleware to parse JSON & URL-encoded bodies * //
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
+
+// * handle reqs for static files, assuming vite builds to Client/dist ?  * //
+app.use(express.static(path.resolve(import.meta.dirname, '../../Client/dist'))); 
+
+
+// * error handling for DB queries * //
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'An unexpected error occurred!' });
+});
+
+
+// * route handlers * //
+
+// fetch a user by ID 
+app.get('/api/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        
+    }
+})
+
+
+// create a new user *** how to integrate with supabase auth for secure user creation??? 
+
+
+
+// add a friend
+
+
+
+// get friends of a user
+
+
+
+// create a new group and add creator as admin
+
+
+
+// join a group
+
+
+// get groups for a user
+
+
+
+// create an expense, split (equally?) among group members, and update splits/balances
+
+
+
+// get expenses for a group
+
+
+
+// get balances for a user (aggregated)
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+}) 
